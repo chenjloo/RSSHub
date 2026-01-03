6777f7de3eb8b4ae9f82a80a13ef82601965a066
chenjloo <40300960+chenjloo@users.noreply.github.com>
2026-01-03 14:48:34 +0800

    Update utils.tsx

--- lib/routes/hk01/utils.tsx
+++ lib/routes/hk01/utils.tsx
@@ -6,91 +6,6 @@ import { parseDate } from '@/utils/parse-date';
 const rootUrl = 'https://hk01.com';
 const apiRootUrl = 'https://web-data.api.hk01.com';
 
-const renderDescription = ({ image, teasers, blocks }) =>
-    renderToString(
-        <>
-            {image ? <img src={image} /> : null}
-            {teasers?.length ? (
-                <backquote>
-                    {teasers.map((teaser) => (
-                        <p>{teaser}</p>
-                    ))}
-                </backquote>
-            ) : null}
-            {blocks?.length
-                ? blocks.map((block) => {
-                      if (block.blockType === 'summary') {
-                          return (
-                              <backquote>
-                                  {block.summary?.map((summary) => (
-                                      <p>{summary}</p>
-                                  ))}
-                              </backquote>
-                          );
-                      }
-
-                      if (block.blockType === 'text') {
-                          return block.htmlTokens?.map((tokens) =>
-                              tokens.map((token) => {
-                                  if (token.type === 'text') {
-                                      return <p>{token.content}</p>;
-                                  }
-
-                                  if (token.type === 'link') {
-                                      return <a href={token.link}>{token.content}</a>;
-                                  }
-
-                                  if (token.type === 'boldText') {
-                                      return <b>{token.content}</b>;
-                                  }
-
-                                  if (token.type === 'boldLink') {
-                                      return (
-                                          <a href={token.link}>
-                                              <b>{token.content}</b>
-                                          </a>
-                                      );
-                                  }
-
-                                  return null;
-                              })
-                          );
-                      }
-
-                      if (block.blockType === 'quote') {
-                          return (
-                              <q>
-                                  {block.message} —— {block.author}
-                              </q>
-                          );
-                      }
-
-                      if (block.blockType === 'image') {
-                          const { image: blockImage } = block;
-
-                          return blockImage ? (
-                              <figure>
-                                  <figurecaption>{blockImage.caption}</figurecaption>
-                                  <img src={blockImage.cdnUrl} alt={blockImage.caption} height={blockImage.originalHeight} width={blockImage.originalWidth} />
-                              </figure>
-                          ) : null;
-                      }
-
-                      if (block.blockType === 'gallery') {
-                          return block.images?.map((blockImage) => (
-                              <figure>
-                                  <figurecaption>{blockImage.caption}</figurecaption>
-                                  <img src={blockImage.cdnUrl} alt={blockImage.caption} height={blockImage.originalHeight} width={blockImage.originalWidth} />
-                              </figure>
-                          ));
-                      }
-
-                      return null;
-                  })
-                : null}
-        </>
-    );
-
 const ProcessItems = (items, limit, tryGet) =>
     Promise.all(
         items
@@ -110,14 +25,16 @@ const ProcessItems = (items, limit, tryGet) =>
                         url: item.link,
                     });
 
-                    const content = JSON.parse(detailResponse.data.match(/"__NEXT_DATA__" type="application\/json">({"props":.*})<\/script>/)[1]);
+                    const content = load(detailResponse.data);
 
-                    item.description = renderDescription({
-                        image: content.props.initialProps.pageProps.article.originalImage.cdnUrl,
-                        teasers: content.props.initialProps.pageProps.article.teaser,
-                        blocks: content.props.initialProps.pageProps.article.blocks,
-                    });
+                    const articleContent = content('#article-content-section')
+                                               .children('div').remove().end()
+                                               .find('div.cmp-icon').remove().end()
+                                               .html();
 
+
+                    item.description = articleContent;
+                    
                     return item;
                 })
             )
