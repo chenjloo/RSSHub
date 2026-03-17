918f0c5de503b2184594cb82cfb3d6002a2d19b6
chenjloo <40300960+chenjloo@users.noreply.github.com>
2026-03-17 21:06:31 +0800

    Create namespace.ts

---
+++ lib/routes/cacs-mofcom/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '中国贸易救济信息网',
+    url: 'cacs.mofcom.gov.cn',
+    description: '商务部贸易救济信息网，提供反倾销、反补贴、保障措施等贸易救济案件信息',
+    zh: {
+        name: '中国贸易救济信息网',
+    },
+};
